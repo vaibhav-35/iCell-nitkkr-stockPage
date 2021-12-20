3a4febf3a73f8a6fd7776920cd1c8a81268f657e
vaibhav-35 <77327065+vaibhav-35@users.noreply.github.com>
2021-12-20 13:07:20 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -10,9 +10,9 @@ function Card(props){
                 <p>{props.desc}</p>
             </div>
             <div className='Card-links'>
-                <a href={props.fb}><img src='Icons/fb.png'></img></a>
-                <a href={props.insta}><img src='Icons/insta.png'></img></a>
-                <a href={props.twitter}><img src='Icons/twitter.png'></img></a>
+                <a href={props.fb}><img src='Icons/fb.png' alt='img'></img></a>
+                <a href={props.insta}><img src='Icons/insta.png' alt='img'></img></a>
+                <a href={props.twitter}><img src='Icons/twitter.png' alt='img'></img></a>
                 <a href={props.yt}><img src='Icons/yt.png'></img></a>
             </div>
         </div>
